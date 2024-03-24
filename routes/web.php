fc424082f6eb4cbbf2c56ca520d760b7058533df
achmad syahrul faroh <syahrulfrh@gmail.com>
2024-03-24 22:21:59 +0700

    jobsheet 2 routing

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,37 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return 'Hello World';
 });
+
+Route::get('/world', function(){
+    return 'world';
+});
+
+Route::get('/', function(){
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function(){
+    return '2241720259 ACHMAD SYAHRUL FAROH';
+});
+
+Route::get('/user/{syahrul}', function($syahrul ){
+    return 'Nama Saya ' . $syahrul;
+});
+
+Route::get('/posts/{post}/comments/{coment}', function($postId,$comentId ){
+    return 'Pos ke-'. $postId." komentar ke-:" . $comentId;
+});
+
+Route::get('/articles/{id}', function($id) {
+    return 'Halaman artikel dengan ' . $id;
+});
+
+Route::get('/user/{name?}', function($name='jhon') {
+    return 'Nama Saya ' . $name;
+});
+
+Route::get('/user/profile', function() {
+})->name('profile');
